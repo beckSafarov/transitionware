f550a41b55d55e4f8b1588dda2329389b1c2d79b
Begzod Safarov <begzod.safarov07@gmail.com>
2022-11-22 17:40:29 +0500

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 connectDB()
 const nodeEnv = process.env.NODE_ENV
-
+const __dirname = path.resolve()
 if (nodeEnv === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.get('*', (req, res) =>
